e3e19e1aa89d4a45a125c4b5d9dc41cafc38aa17
Siwon Yun <ysw421@naver.com>
2025-11-27 21:18:18 +0900

    add hjkl

--- src/core/ui.c
+++ src/core/ui.c
@@ -4,9 +4,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+void draw_border(WINDOW *win, int color_pair) {
+    wattron(win, COLOR_PAIR(color_pair));
+    box(win, 0, 0);
+    wattroff(win, COLOR_PAIR(color_pair));
+}
+
 void print_lines(WINDOW *win, char **lines, int num_lines, int top_line, int highlight, int win_height, int win_width) {
     werase(win);
-    box(win, 0, 0);
     for (int i = 0; i < win_height - 2; ++i) {
         int current_line = top_line + i;
         if (current_line >= num_lines) {
@@ -24,9 +29,24 @@ void print_lines(WINDOW *win, char **lines, int num_lines, int top_line, int hig
 }
 
 int start_ui(const char* git_log_filepath) {
-    if (initscr() == NULL) {
+    if (initscr() == NULL) return 1;
+    if (has_colors() == FALSE) {
+        endwin();
+        printf("Your terminal does not support color\n");
         return 1;
     }
+
+    start_color();
+    use_default_colors();
+    init_pair(1, COLOR_RED, -1);
+    init_pair(2, COLOR_GREEN, -1);
+    init_pair(3, COLOR_YELLOW, -1);
+    init_pair(4, COLOR_BLUE, -1);
+    init_pair(5, COLOR_MAGENTA, -1);
+    init_pair(6, COLOR_CYAN, -1);
+    init_pair(7, COLOR_WHITE, -1);
+    init_pair(8, COLOR_GREEN, -1);
+
     clear();
     cbreak();
     noecho();
@@ -45,7 +65,7 @@ int start_ui(const char* git_log_filepath) {
 
     WINDOW *left_win = newwin(height, width / 2, 0, 0);
     WINDOW *right_win = newwin(height, width / 2, 0, width / 2);
-    box(right_win, 0, 0);
+    
     mvwprintw(right_win, 1, 1, "Welcome");
     wrefresh(right_win);
 
@@ -76,43 +96,44 @@ int start_ui(const char* git_log_filepath) {
 
     int highlight = 0;
     int top_line = 0;
+    int active_window = 0;
     int ch;
 
-    print_lines(left_win, lines, num_lines, top_line, highlight, height, width / 2);
-
     while ((ch = getch()) != 'q') {
-        switch (ch) {
-            case KEY_UP:
-            case 'k':
-                if (highlight > 0) {
-                    highlight--;
-                    if (highlight < top_line) {
-                        top_line = highlight;
+        if (active_window == 0) {
+            switch (ch) {
+                case KEY_UP:
+                case 'k':
+                    if (highlight > 0) {
+                        highlight--;
+                        if (highlight < top_line) top_line = highlight;
                     }
-                }
-                break;
-            case KEY_DOWN:
-            case 'j':
-                if (highlight < num_lines - 1) {
-                    highlight++;
-                    if (highlight >= top_line + height - 2) {
-                        top_line = highlight - (height - 3);
+                    break;
+                case KEY_DOWN:
+                case 'j':
+                    if (highlight < num_lines - 1) {
+                        highlight++;
+                        if (highlight >= top_line + height - 2) top_line = highlight - (height - 3);
                     }
-                }
-                break;
-            case 'h':
-                 highlight -= (height - 2);
-                 if (highlight < 0) highlight = 0;
-                 top_line = highlight;
-                 break;
-            case 'l':
-                 highlight += (height - 2);
-                 if (highlight >= num_lines) highlight = num_lines - 1;
-                 top_line = highlight - (height - 3);
-                 if (top_line < 0) top_line = 0;
-                 break;
+                    break;
+            }
         }
+        
+        switch (ch) {
+            case KEY_RIGHT:
+            case 'l':
+                active_window = 1;
+                break;
+            case KEY_LEFT:
+            case 'h':
+                active_window = 0;
+                break;
+        }
+
+        draw_border(left_win, (active_window == 0) ? 8 : 7);
+        draw_border(right_win, (active_window == 1) ? 8 : 7);
         print_lines(left_win, lines, num_lines, top_line, highlight, height, width / 2);
+        wrefresh(right_win);
     }
 
     for (int i = 0; i < num_lines; ++i) {
